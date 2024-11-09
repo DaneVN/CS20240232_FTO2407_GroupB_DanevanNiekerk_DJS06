b4ae1d49384819d186c5fc7dbe145a8cb2b1b556
DaneVN <Dane.vN23@gmail.com>
2024-11-09 17:17:15 +0200

    Initial Files Setup

---
+++ src/scripts.js
@@ -0,0 +1,45 @@
+import {} from "../exercises.js";
+// A list of provinces:
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
+];
+
+// A list of names:
+const names = [
+  "Ashwin",
+  "Sibongile",
+  "Jan-Hendrik",
+  "Sifso",
+  "Shailen",
+  "Frikkie",
+];
+
+// A list of products with prices:
+const products = [
+  { product: "banana", price: "2" },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: "8" },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
+
+// Exercises
+console.log("CHALLENGE 1");
+
+console.log("CHALLENGE 2");
+
+console.log("CHALLENGE 3");
+
+console.log("CHALLENGE 4");
+
+console.log("CHALLENGE 5");
+
+console.log("CHALLENGE 6");
+
+console.log("CHALLENGE 7");
